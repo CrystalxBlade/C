66903888f0e801622c771aa959741917bc007dbc
Satish <Satish141m@gmail.com>
2024-07-18 07:51:12 -0700

    Initial commit

---
+++ Maths/AreaRect.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int l;
+    int b;
+
+    printf("Enter the length: ");
+    scanf("%d",&l);
+
+    printf("Enter the breadth: ");
+    scanf("%d",&b);
+
+    printf("Area of Reactangle %d", l*b);
+    return 0;
+}
