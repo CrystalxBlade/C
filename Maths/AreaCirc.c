8c004f4fc0a97764ad78274c0e4179cd705cd740
Satish <Satish141m@gmail.com>
2024-07-18 08:21:33 -0700

    Update AreaCirc.c

--- Maths/AreaCirc.c
+++ Maths/AreaCirc.c
@@ -4,7 +4,7 @@ int main()
 {
     int r = 6;
 
-    printf("The area of circle with radius %d is %d",r,3.14 * r * r);
+    printf("The area of circle with radius %d is %f",r,3.14 * r * r);
 
     return 0;
 }
